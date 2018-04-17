9b8c99b4928878c83b07f8aa5ee25043b462d83f
Jorge Rodriguez <jorge.rodriguez@webmaps.com.mx>
2018-04-17 17:38:51 -0500

    Bottom Navigation con onNavigationItemSelected

--- app/src/main/java/mx/com/webmaps/md_ejercicio8/MainActivity.java
+++ app/src/main/java/mx/com/webmaps/md_ejercicio8/MainActivity.java
@@ -1,13 +1,51 @@
 package mx.com.webmaps.md_ejercicio8;
 
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Switch;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationItemView;
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationItemView = (BottomNavigationView) findViewById(R.id.bottomNavigation_id);
+        txt = (TextView) findViewById(R.id.text_id);
+
+        bottomNavigationItemView.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id= item.getItemId();
+
+        switch (id){
+            case R.id.recents_id:{
+                txt.setText(getResources().getText(R.string.recents));
+                return  true;
+            }
+            case R.id.favorites_id:{
+                txt.setText(getResources().getText(R.string.favorites));
+                return  true;
+            }
+            case R.id.nearby_id:{
+                txt.setText(getResources().getText(R.string.near_by));
+                return  true;
+            }
+        }
+
+
+        return false;
     }
 }
